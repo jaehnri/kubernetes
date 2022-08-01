e46fa9ded306f4bcb6fbb3992abb31915fe7f89e
Dixita Narang <ndixita@google.com>
2022-08-01 19:21:30 +0000

    Renaming usage of v1beta1 to v1, and adding API violation exceptions and vendor module for v1

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -449,6 +449,7 @@ const (
 	// owner: @andrewsykim @adisky
 	// alpha: v1.20
 	// beta: v1.24
+	// ga: v1.25
 	//
 	// Enable kubelet exec plugins for image pull credentials.
 	KubeletCredentialProviders featuregate.Feature = "KubeletCredentialProviders"
@@ -911,7 +912,7 @@ var defaultKubernetesFeatureGates = map[featuregate.Feature]featuregate.FeatureS
 
 	JobTrackingWithFinalizers: {Default: true, PreRelease: featuregate.Beta},
 
-	KubeletCredentialProviders: {Default: true, PreRelease: featuregate.Beta},
+	KubeletCredentialProviders: {Default: true, PreRelease: featuregate.GA},
 
 	KubeletInUserNamespace: {Default: false, PreRelease: featuregate.Alpha},
 
